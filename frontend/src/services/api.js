c36c5169eb5d9659160a6c8fef936dfb1d92c9d5
thanusreethiruveedhi <thanusreethiruveedhi@gmail.com>
2026-07-02 12:28:17 +0000

    Update API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL:
-    "https://supreme-space-computing-machine-q7r5xxxrwjrph9657-5000.app.github.dev/api",
+  baseURL: "https://sbstocks-backend.onrender.com/api",
 });
 
 export default api;
